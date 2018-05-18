5f0538da861d11d142f60dab639df4ca616637ac
Veremeyenko Sergii <veremeyenko-s@yandex.ru>
2018-05-18 11:48:05 +0300

    Add project files.

---
+++ Coupon/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Coupon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
